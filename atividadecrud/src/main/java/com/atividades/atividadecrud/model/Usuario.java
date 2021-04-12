9db53a77eed2af30d340e28c6c546073ff0bada5
Maurício <mauricio_nobhre@hotmail.com>
2021-04-12 15:19:01 -0300

    Atividade CRUD Atualizada com Swagger

--- atividadecrud/src/main/java/com/atividades/atividadecrud/model/Usuario.java
+++ atividadecrud/src/main/java/com/atividades/atividadecrud/model/Usuario.java
@@ -26,7 +26,6 @@ public class Usuario {
 	private String usuario;
 
 	@NotNull
-	@Size (min = 5, max = 100)
 	private String senha;
 	
 	//GetSet
--- bancodedados/src/test/java/com/integracao/bancodedados/BancodedadosApplicationTests.java
+++ bancodedados/src/test/java/com/integracao/bancodedados/BancodedadosApplicationTests.java
@@ -1,10 +1,10 @@
-package com.atividades.atividadecrud;
+package com.integracao.bancodedados;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AtividadeCrudApplicationTests {
+class BancodedadosApplicationTests {
 
 	@Test
 	void contextLoads() {
